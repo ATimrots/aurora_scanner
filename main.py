766fd4697926a5bcf76f6f781e714f7ae8fa0008
Artūrs Timrots <arturs@migbaltic.lv>
2023-08-25 12:28:40 +0300

    Avoid problems of clicking element on virtual display

--- main.py
+++ main.py
@@ -59,7 +59,6 @@ print('Url: '+driver.current_url)
 
 img_src = driver.find_element(By.XPATH, '//*[@id="alaska"]').get_attribute('src')
 to_europe = driver.find_element(By.XPATH, '//*[@id="eu-map"]')
-next_button = driver.find_element(By.XPATH, '//*[@id="right-nav"]')
 current_date = driver.find_element(By.ID, 'local-date').text
 
 driver.execute_script("arguments[0].click();", to_europe)
@@ -69,9 +68,12 @@ img_elem = wait.until(attribute_has_changed((By.XPATH, '//*[@id="alaska"]'), img
 
 print('Image of opened region: '+img_elem.get_attribute('src'))
 
-next_button.click()
+next_button = driver.find_element(By.XPATH, '//*[@id="right-nav"]')
+driver.execute_script("arguments[0].click();", next_button)
+
 wait = WebDriverWait(driver, 5)
 today_date = wait.until(attribute_has_changed((By.ID, 'local-date'), current_date, 'text')).text
+
 kp_index = driver.find_element(By.XPATH, '//*[@id="kp_value"]').text
 
 print("EUROPE KP INDEX: "+kp_index+' ON "'+today_date+'"')
@@ -86,7 +88,7 @@ else:
     date_text = today_date
 
     for i in range(7):
-        next_button.click()
+        driver.execute_script("arguments[0].click();", next_button)
         wait = WebDriverWait(driver, 5)
         date = wait.until(attribute_has_changed((By.ID, 'local-date'), date_text, 'text'))
         date_text = date.text
@@ -98,5 +100,8 @@ else:
             print(message)
             ntfy(message, 'calendar')
             break
+
+if APP_ENV == 'production':
+    display.stop()
 
 driver.quit()
