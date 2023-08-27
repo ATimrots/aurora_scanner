3791629aceefd039ccf6a18a3d7c60ba9fff8387
Artūrs Timrots <arturs@migbaltic.lv>
2023-08-27 20:32:40 +0300

    Make ntfy topic configurable

--- ntfy.py
+++ ntfy.py
@@ -1,12 +1,20 @@
 import requests
+import os
 
 def ntfy(message, tag = 'star_struck'):
     """
     See https://docs.ntfy.sh/ for more details
     """
+
+    topic = os.getenv('NTFY_TOPIC')
+
+    # Do not rise an error, but let it print out result without sending notification
+    if topic == "" or topic == None:
+        return
+
     h = {
         "Tags": tag,
         # "Actions": "view, Open portal, https://www.gi.alaska.edu/monitors/aurora-forecast"
     }
 
-    requests.post("https://ntfy.sh/atimrots-aurora-alerts", data=message, headers=h)
+    requests.post("https://ntfy.sh/"+topic, data=message, headers=h)
